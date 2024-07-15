dec35ec09bf4994a2c2368fa55f540c99334a9e4
Jayveer Rathwa <jaay148635@gmail.com>
2024-07-15 17:28:28 +0530

    add image upload to edit

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const {storage} = require('../cloudinary');
+const upload = multer({storage});
 const campgrounds =require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, validateCampground, isAuthor} = require('../middleware');
@@ -7,13 +10,13 @@ const {isLoggedIn, validateCampground, isAuthor} = require('../middleware');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
